8b79d0c44e94ea72f2eb84b0dbeabf4b87fed7f0
MazingerZ62 <lenaick.oudard@monlycee.net>
2026-01-23 17:25:56 +0000

    creation site 23/01/2026

---
+++ Mon site/script_site.js
@@ -0,0 +1,19 @@
+let boutonadd = document.getElementById("addbtn");
+    boutonadd.addEventListener("click",function(){
+        window.location.href = "./Mon site/page_addition.html"
+    });
+
+let boutonsous = document.getElementById("sousbtn");
+    boutonsous.addEventListener("click",function(){
+        window.location.href = "./Mon site/page_soustraction.html"
+    });
+
+let boutonmul = document.getElementById("mulbtn");
+    boutonmul.addEventListener("click",function(){
+        window.location.href = "./Mon site/page_multiplication.html"
+    });
+
+let boutondiv = document.getElementById("divbtn");
+    boutondiv.addEventListener("click",function(){
+        window.location.href = "./Mon site/page_division.html"
+    });
